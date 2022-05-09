b15a9d02c85787beb5e5c681b8f1b10fe8aa05a0
Gnarcoin <max24790@hotmail.com>
2022-05-09 18:40:20 +0000

    changes made from theia lab environment

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -96,7 +96,7 @@ class Enrollment(models.Model):
 
 class Question(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    content = models.CharField(max_length=200)
+    content = models.CharField(max_length=200, default='Question default')
     grade = models.IntegerField(default=50)
 
     def __str__(self):
